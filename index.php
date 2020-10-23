8b3fe54d499265b6a54ff58ccfdeff0af4955a3a
titivince <72084738+titivince@users.noreply.github.com>
2020-10-23 09:05:29 +0200

    comment code

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
-<?php
+<?php //connection bdd
 $pdo = new PDO('mysql:host=mysql;dbname=basedeteste;host=127.0.0.1', 'root', '', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-$sql = $pdo->query("SELECT * FROM ville ") ;
+$sql = $pdo->query("SELECT * FROM ville"); //request bdd
 
 $villes = $sql->fetchAll();
+
+if(isset($_GET['villes'])) {
+    $cap = $_GET['villes'];
+    $sql = $pdo->prepare("SELECT pays FROM ville WHERE capital = :capital");//prepare sql request
+    $sql->bindParam(":capital", $cap);                           // bind param
+    $sql->execute();         //execute sql request
+    $fetch = $sql->fetch();  //fetch sql request
+    $pays = $fetch['pays'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -20,26 +29,18 @@ $villes = $sql->fetchAll();
                 <div class="form-group">
                     <label>Quelles villes choississez-vous ?</label>
                     <select name="villes" class="form-control form-control-sm">
-                        <?php foreach ($villes as $list): ?>
-                        <option name="<?= $list['capital'] ?>"><?= $list['capital'] ?></option>
-                        <?php endforeach; ?>
+                        <?php foreach ($villes as $list): ?>                    //create select with bdd
+                            <option name="<?= $list['capital'] ?>"> <?= $list['capital'] ?> </option>
+                        <?php $list['pays'];
+                        endforeach; ?>
                     </select>
                 </div>
                 <input type="submit" class="btn btn-outline-primary">
             </form><br>
-            <?php $ville = $_GET['villes'];
-                if ($ville == "Paris") {
-                $pays = "de la France";
-                echo $ville . ' est la capitale ' . $pays;
-                } elseif ($ville == 'Rome') {
-                $pays = "Italie";
-                echo $ville . " est la capitale de l'" . $pays;
-                } elseif ($ville == 'Tokyo') {
-                $pays = 'du Japon';
-                echo $ville . ' est la capitale ' . $pays;
-                } else {
-                echo 'Une réponce ';
-                }
+            <?php
+            if(isset($_GET['villes'])) {            //print result
+                echo "La $pays a pour capital $cap";
+            }
             ?>
         </div>
     </body>
